6cf1b090d6ed7e24817123c5dc672850841cd09d
zozo9109 <63043820+zozo9109@users.noreply.github.com>
2026-04-27 14:46:03 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.02 ms, Memory: 82.6 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int slice, int n) {
+        // int answer = 0;
+        
+        return ((slice + n - 1) / slice);
+        
+//         while(
+        
+        
+        
+//         // 피자를 2조각 ~ 10조각 조각 수 잘라줌
+//         // 피자 조각 * i = n -> 1.xxx가 되면 됨
+//         for(int i = 0; i < n; i++){
+//             answer = slice * i; //4 8 12 16 ...
+//             if(answer/n == 1){
+//                 pizza = i;
+//                 break;
+//             }
+//         }
+//         return pizza;
+   }
+}
